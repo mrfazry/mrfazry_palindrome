b37ea1dca95ebe8e519d2a5d224f7f159ddf14c7
mrfazry <mredykafazry@gmail.com>
2018-11-27 17:02:16 +0700

    refactor palindrome method

--- lib/mrfazry_palindrome.rb
+++ lib/mrfazry_palindrome.rb
@@ -1,6 +1,14 @@
-require "mrfazry_palindrome/version"
+# require "mrfazry_palindrome/version"
 
-module MrfazryPalindrome
-  class Error < StandardError; end
-  # Your code goes here...
+class String
+
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
+  end
 end
--- test/mrfazry_palindrome_test.rb
+++ test/mrfazry_palindrome_test.rb
@@ -1,11 +1,28 @@
 require "test_helper"
 
 class MrfazryPalindromeTest < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::MrfazryPalindrome::VERSION
+  # def test_that_it_has_a_version_number
+  #   refute_nil ::MrfazryPalindrome::VERSION
+  # end
+  #
+  # def test_it_does_something_useful
+  #   assert true
+  # end
+
+  def test_non_palindrome
+    refute "apple".palindrome?
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_literal_palindrome
+    assert "racecar".palindrome?
   end
+
+  def test_mixed_case_palindrome
+    assert "RaceCar".palindrome?
+  end
+
+  def test_palindrome_with_punctuation
+    assert "Madam, I'm Adam.".palindrome?
+  end
+
 end
